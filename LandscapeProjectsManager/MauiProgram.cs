9c1cb78df6c13ccb9a9f49d1ae113e5426464b75
Saule Abramoviciute <abramoviciutesaule@gmail.com>
2024-02-28 18:55:46 +0100

    Added a calendar

--- LandscapeProjectsManager/MauiProgram.cs
+++ LandscapeProjectsManager/MauiProgram.cs
@@ -1,24 +1,23 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Syncfusion.Maui.Core.Hosting;
 
-namespace LandscapeProjectsManager;
-
-public static class MauiProgram
+namespace LandscapeProjectsManager
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+			builder.ConfigureSyncfusionCore();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Pompiere-Regular.ttf", "TitleFont");
+                });
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-
-		return builder.Build();
-	}
+            return builder.Build();
+        }
+    }
 }
