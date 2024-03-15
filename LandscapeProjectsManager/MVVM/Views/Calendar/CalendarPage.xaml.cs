3a6376618eb0a541b2c91869be1df38077b6e738
Saule Abramoviciute <abramoviciutesaule@gmail.com>
2024-03-15 14:42:43 +0100

    Filter events buttons added

--- LandscapeProjectsManager/MVVM/Views/Calendar/CalendarPage.xaml.cs
+++ LandscapeProjectsManager/MVVM/Views/Calendar/CalendarPage.xaml.cs
@@ -36,5 +36,63 @@ namespace LandscapeProjectsManager
             var modalPage = new AddEventPage(selectedDateTime, viewModel);
             await Navigation.PushModalAsync(modalPage);
         }
+
+        private void yellowCircle_Clicked(object sender, EventArgs e)
+        {
+            yellowEmptyCircle.IsEnabled = true;
+            yellowEmptyCircle.IsVisible = true;
+            yellowCircle.IsVisible = false;
+            yellowCircle.IsEnabled = false;
+            // ImageButton button = (ImageButton)sender;
+            // 
+            // if (button.Source.ToString() == "FileImageSource{yellowcircle.png}")
+            // {
+            //     button.Source = ImageSource.FromFile("yellowemptycircle.png");
+            // }
+            // else if (button.Source.ToString() == "FileImageSource{yellowemptycircle.png}")
+            // {
+            //     button.Source = ImageSource.FromFile("yellowcircle.png");
+            // }
+        }
+
+        private void yellowEmptyCircle_Clicked(object sender, EventArgs e)
+        {
+            yellowEmptyCircle.IsEnabled = false;
+            yellowEmptyCircle.IsVisible = false;
+            yellowCircle.IsVisible = true;
+            yellowCircle.IsEnabled = true;
+        }
+
+        private void blueCircle_Clicked(object sender, EventArgs e)
+        {
+            blueEmptyCircle.IsEnabled = true;
+            blueEmptyCircle.IsVisible = true;
+            blueCircle.IsVisible = false;
+            blueCircle.IsEnabled = false;
+        }
+
+            private void blueEmptyCircle_Clicked(object sender, EventArgs e)
+        {
+            blueEmptyCircle.IsEnabled = false;
+            blueEmptyCircle.IsVisible = false;
+            blueCircle.IsVisible = true;
+            blueCircle.IsEnabled = true;
+        }
+
+        private void redCircle_Clicked(object sender, EventArgs e)
+        {
+            redEmptyCircle.IsEnabled = true;
+            redEmptyCircle.IsVisible = true;
+            redCircle.IsVisible = false;
+            redCircle.IsEnabled = false;
+        }
+
+        private void redEmptyCircle_Clicked(object sender, EventArgs e)
+        {
+            redEmptyCircle.IsEnabled = false;
+            redEmptyCircle.IsVisible = false;
+            redCircle.IsVisible = true;
+            redCircle.IsEnabled = true;
+        }
     }
 }
