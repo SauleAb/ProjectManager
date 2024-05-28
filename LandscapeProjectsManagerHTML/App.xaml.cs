5c6580bfa33930cd13b8be9140df7ae295d0f4a9
Saule Abramoviciute <abramoviciutesaule@gmail.com>
2024-05-28 15:05:49 +0200

    .

---
+++ LandscapeProjectsManagerHTML/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LandscapeProjectsManagerHTML
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
